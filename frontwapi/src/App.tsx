55f6e076544a34f12f1ed8aa357098df3c139c98
phbrugnolo <phbrugnolo@gmail.com>
2024-05-27 19:30:20 -0300

    Arrumando rota

---
+++ frontwapi/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProdutoListar from "./components/pages/produto/produto-listar";
+
+function App() {
+  return (
+    <div className="App">
+      <ProdutoListar></ProdutoListar>
+    </div>
+  );
+}
+
+export default App;
